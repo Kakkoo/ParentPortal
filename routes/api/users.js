a25efb2ed56378e6d47477ec6f34eb9d08edc5c8
Kanchan Sharma <58194538+Kakkoo@users.noreply.github.com>
2022-03-01 10:43:02 -0800

    lil change

--- routes/api/users.js
+++ routes/api/users.js
@@ -54,7 +54,7 @@ router.post("/register", (req, res) => {
           newUser
             .save()
             .then((user) => res.json(user))
-            .catch((err) => console.log(err));
+            .catch((err) => console.log(err + "yes"));
         });
       });
     }
