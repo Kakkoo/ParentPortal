d8094032c9adf57ccecbb019076cb110cffc3fd1
Kanchan Sharma <58194538+Kakkoo@users.noreply.github.com>
2022-03-02 10:02:32 -0800

    port changes

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 
 //First route
 app.get('/', (req, res) => res.send('Hello World'));
+const port = process.env.PORT || 7200;
 
-const port = 7200;
+//const port = 7200;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
