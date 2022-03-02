13305064fa6e758560f5b5082e76efcb0fda84ac
Kanchan Sharma <58194538+Kakkoo@users.noreply.github.com>
2022-03-02 15:01:11 -0800

    new change

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI:
+      "mongodb+srv://parentportal:parPAR098)(*@parentportaldata.81lat.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    smtp: "smtps://lionvihaan@gmail.com:wyspsslrrbpxnlse@smtp.gmail.com",
+  };
