69ddcdeefbc03f82a46e156833a5ccb66bb2b4d3
Kanchan Sharma <58194538+Kakkoo@users.noreply.github.com>
2022-03-02 12:53:33 -0800

    api added

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const users = require('./routes/api/users');
-const kids = require('./routes/api/kids');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
-const passport = require('passport');
-const nodemailer = require("nodemailer");
-const lodash = require("lodash");
 const app = express();
 
-//Body parser configuration redeploy
+//Body parser configuration
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 //First route
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello World'));
 
 //Use routes
 app.use('/api/users', users);
-app.use('/api/kids', kids);
-const port = 7200;
+
+const port = process.env.port  || 7200;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 //Db config
@@ -30,7 +26,7 @@ mongoose
   .then(() => console.log('MongoDb Connected'))
   .catch((err) => console.log(err));
 
-app.use(passport.initialize());
 
-require('./config/passport')(passport);
+
+
 
